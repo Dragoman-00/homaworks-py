ff877b7b6f15e2e8fec888d0d4e34ab41230affd
God of WAR <vovadragoman@mail.ru>
2024-05-17 10:40:42 +0300

    works

---
+++ projects 1 lesson2.py
@@ -0,0 +1,10 @@
+ves_zakaza = 4.5 # 4.5 kg
+stoimocti_chereshni = 34 # 34 rub\kg
+dengi = 1000
+print(dengi-(ves_zakaza*stoimocti_chereshni))
+
+tovar_one = 3 # 3 kg
+stoimocti_tone = 30 # 30rub\kg
+stoimocti_ttwo = 23 # 23 rub\kg
+tovar_two = 6 # 6 kg
+print(dengi-((ves_zakaza*stoimocti_chereshni)+(tovar_two*stoimocti_ttwo)+(tovar_one*stoimocti_tone)))
