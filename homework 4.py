2aacfc4e4607e8ed4ef5cd302c8b62796b589953
God of WAR <vovadragoman@mail.ru>
2024-05-18 21:51:53 +0300

    works

---
+++ homework 4.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, True, "good")
+print(immutable_var)
+#immutable_var[0] = 3 изменить данные в картеже нельзя
+mutable_list = [1, 2, True, "Nice"]
+mutable_list[1] = 3
+mutable_list[3] = "i am good"
+print(mutable_list)
