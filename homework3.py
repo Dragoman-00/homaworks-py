41ad54baae208cc6537baec876f83f3430034450
God of WAR <vovadragoman@mail.ru>
2024-05-18 14:11:39 +0300

    works

---
+++ homework3.py
@@ -0,0 +1,6 @@
+my_string = input("Сколько символов? ")
+print(len(my_string))
+print(my_string .upper()) # print(my_string .lower())
+print("hi man! how are you?" .replace(" " , ""))
+print("hi man! how are you?" .replace("hi man! how are you?", "h"))
+print("hi man! how are you?" .replace("hi man! how are you?", ""))
