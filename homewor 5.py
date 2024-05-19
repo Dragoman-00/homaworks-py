ff2e0cd10c21e9a3e144e6859d9df95a5ebb48e7
God of WAR <vovadragoman@mail.ru>
2024-05-19 13:29:45 +0300

    works

---
+++ homewor 5.py
@@ -0,0 +1,9 @@
+my_list = ["apple", 'cherry', "mango", "coconut", 'kiwi', "banana"]
+print(my_list)
+my_list.remove("apple")
+my_list.remove("banana")
+print(my_list)
+
+my_list.remove("cherry")
+my_list.remove("mango")
+print(my_list)
--- homework2.py
+++ homework2.py
@@ -2,5 +2,5 @@ name = "Vova"
 print(name)
 age = 23
 print(age + 1)
-is_student = "True"
+is_student = True
 print(is_student)
