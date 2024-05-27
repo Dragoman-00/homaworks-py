b01099669513be19f8714a5180cbf27e69bbb6c7
God of WAR <vovadragoman@mail.ru>
2024-05-27 22:54:54 +0300

    works

---
+++ MODUL 2 Z1.py
@@ -0,0 +1,10 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second == third:
+    print(3)
+elif first == second or first == third:
+    print(2)
+elif second == first or second == third:
+    print(2)
+print(0)
--- project 2.py
+++
@@ -1,4 +0,0 @@
-name ="радар"
-name1 = "норма"
-print(name[::-1])
-print(name1[::-1])
