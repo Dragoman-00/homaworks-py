6653f28efbc0d411a8d842f56d4af1be5a0cdf9e
God of WAR <vovadragoman@mail.ru>
2024-07-13 13:22:28 +0300

    works

--- class l1.py
+++ class l1.py
@@ -14,6 +14,34 @@ class House:
         return self.number_of_floors
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+
+
 
 
 h1 = House('ЖК Горский', 18)
@@ -30,3 +58,30 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+
+
+print("---------------------------------------------------------------------------------")
+print("Перегрузка операторов.")
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print("---------------------------------------------------------------------------------")
+print(h1 == h2) # __eq__
+print("Проверка add")
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+print("-------------------")
+print("radd and iadd")
+h1 += 10 # __iadd__
+print(h1)
+h2 = 10 + h2 # __radd__
+print(h2)
+print("-------------------")
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+print("-------------------")
